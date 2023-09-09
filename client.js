a106da30e9ff2b9ea300f02357f9d055af21b90a
Coach Chuck <christian.krueger.health@gmail.com>
2023-09-09 16:17:33 -0400

    done

---
+++ client.js
@@ -0,0 +1,267 @@
+const WebSocket = require("ws");
+const axios = require("axios");
+const { relayInit } = require("nostr-tools");
+const { sleep } = require("./lib/helpers")
+const { OFFERING_KIND } = require('./lib/defines')
+
+require("dotenv").config();
+global.WebSocket = WebSocket;
+
+// ----------------- HELPERS ---------------------------
+
+function getLatestEventByService(events, desiredService) {
+  return events
+    .filter((event) => {
+      const serviceTag = event.tags.find((tag) => tag[0] === "s");
+      return serviceTag && serviceTag[1] === desiredService;
+    })
+    .sort((a, b) => b.created_at - a.created_at)[0];
+}
+
+async function pollUrl(url, runs, delay) {
+  for (let i = 0; i < runs; i++) {
+    try {
+      const response = await axios.get(url);
+
+      if (response.status == 202) {
+        throw new Error("Not ready yet");
+      }
+
+      return response.data;
+    } catch (error) {
+    //   console.error(`Fetching ${url}`);
+      
+      if (i === runs - 1 || error.status == 500) {
+        throw new Error("Poll Timeout");
+      }
+      await sleep(delay);
+    }
+  }
+}
+
+// ----------------- GPT ---------------------------
+
+function parseGPTResponse(response) {
+  if (response.choices && response.choices.length > 0) {
+    const assistantMessage = response.choices[0].message;
+    if (assistantMessage.role === "assistant") {
+      return assistantMessage.content.trim(); // Using trim() to remove any unnecessary whitespace
+    }
+  }
+  return null;
+}
+
+async function runGPT(relay, index, question) {
+  return new Promise(async (resolve, reject) => {
+    // --------------------- Fetch Offering Event -----------------------------
+
+    const postedNoteList = await relay.list([
+      {
+        kinds: [OFFERING_KIND],
+        limit: 10,
+      },
+    ]);
+
+    const postedNote = getLatestEventByService(
+      postedNoteList,
+      "https://api.openai.com/v1/chat/completions"
+    );
+    const postedNoteContent = JSON.parse(postedNote.content);
+
+    // --------------------- Post to Note's endpoint -----------------------------
+
+    const requestData = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: question ?? "Tell me a joke",
+        },
+      ],
+    };
+
+    let responseData;
+    try {
+      const response = await axios.post(
+        postedNoteContent.endpoint,
+        requestData
+      );
+      responseData = response.data;
+    } catch (e) {
+      if (e.response && e.response.status === 402) {
+        responseData = e.response.data;
+      } else {
+        throw new Error(`Bad Request ${e}`);
+      }
+    }
+
+    // --------------------- Pay invoice -----------------------------
+
+    console.log(`------- PAYING GPT ${index} --------`);
+    console.log(responseData.pr);
+    console.log("----------------------------");
+
+    const response = await axios.post(
+      "https://legend.lnbits.com/api/v1/payments",
+      {
+        out: true,
+        bolt11: responseData.pr,
+      },
+      {
+        headers: {
+          "X-Api-Key": process.env.LNBITS_API,
+          "Content-type": "application/json",
+        },
+      }
+    );
+
+    console.log("------- PAID GPT ---------------");
+    console.log(response.data);
+    console.log("----------------------------");
+
+    // --------------------- Poll SuccessAction for Response -----------------------------
+
+    
+
+    const totalResponse = await pollUrl(
+      responseData.successAction.url,
+      99,
+      1000
+    );
+    const gpt = parseGPTResponse(totalResponse);
+
+    console.log(`------- GPT ( ${index} ) ----------------`);
+    console.log(`User: ${question}`);
+    console.log(`${postedNote.s}: ${gpt}`);
+    console.log("----------------------------");
+
+    resolve(gpt);
+  });
+}
+
+// ----------------- STABLE DIFFUSION ---------------------------
+
+async function runStableDiffusion(relay, index, prompt, model) {
+  return new Promise(async (resolve, reject) => {
+    // --------------------- Fetch Offering Event -----------------------------
+
+    const postedNoteList = await relay.list([
+      {
+        kinds: [OFFERING_KIND],
+        limit: 10,
+      },
+    ]);
+
+    const postedNote = getLatestEventByService(
+      postedNoteList,
+      "https://stablediffusionapi.com/api/v4/dreambooth"
+    );
+    const postedNoteData = JSON.parse(postedNote.content);
+
+    // --------------------- Post to Note's endpoint -----------------------------
+
+    const requestData = {
+      model_id: model ?? "landscapev21",
+      prompt: prompt ?? "A puppy",
+      negative_prompt:
+        "painting, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime",
+      width: "512",
+      height: "512",
+      samples: "1",
+      num_inference_steps: "30",
+      safety_checker: "no",
+      enhance_prompt: "yes",
+      seed: index,
+      guidance_scale: 7.5,
+      multi_lingual: "no",
+      panorama: "no",
+      self_attention: "no",
+      upscale: "no",
+      embeddings: "embeddings_model_id",
+      lora: "lora_model_id",
+      webhook: null,
+      track_id: null,
+    };
+
+    let responseData;
+    try {
+      const response = await axios.post(postedNoteData.endpoint, requestData);
+      responseData = response.data;
+    } catch (e) {
+      if (e.response && e.response.status === 402) {
+        responseData = e.response.data;
+      } else {
+        throw new Error(`Bad Request ${e}`);
+      }
+    }
+
+    // --------------------- Pay invoice -----------------------------
+
+    console.log(`------- PAYING SD ${index} --------`);
+    console.log(responseData.pr);
+    console.log("----------------------------");
+
+    const response = await axios.post(
+      "https://legend.lnbits.com/api/v1/payments",
+      {
+        out: true,
+        bolt11: responseData.pr,
+      },
+      {
+        headers: {
+          "X-Api-Key": process.env.LNBITS_API,
+          "Content-type": "application/json",
+        },
+      }
+    );
+
+    console.log("------- PAID SD ---------------");
+    console.log(response.data);
+    console.log("----------------------------");
+
+    // --------------------- Poll SuccessAction for Response -----------------------------
+
+    const totalResponse = await pollUrl(
+      responseData.successAction.url,
+      99,
+      1000
+    );
+
+    console.log("------- IMAGES ---------------");
+    console.log(totalResponse);
+    console.log("----------------------------");
+
+    resolve(totalResponse);
+  });
+}
+
+// --------------------- MAIN -----------------------------
+
+async function main() {
+  const relay = relayInit(process.env.NOSTR_RELAY);
+  relay.on("connect", () => {
+    console.log(`connected to ${relay.url}`);
+  });
+  relay.on("error", (e) => {
+    console.log(`failed to connect to ${relay.url}: ${e}`);
+  });
+  await relay.connect();
+
+  // --------------------- Call Endpoints -----------------------------
+
+  const runs = 3;
+  const gptRuns = [];
+  for (let i = 0; i < runs; i++) {
+    gptRuns.push(runGPT(relay, i, `Tell me a joke about the number ${i}`));
+  }
+  await Promise.all(gptRuns);
+
+//   await runGPT(relay, i, `Tell me a joke`);
+//   await runStableDiffusion(relay, 0, "Cypherpunk girl with purple hair", "toonyou");
+
+  // --------------------- Clean Up -----------------------------
+
+  relay.close();
+}
+
+main();
---
+++ server.js
@@ -0,0 +1,427 @@
+const WebSocket = require("ws");
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const axios = require("axios");
+const bolt11 = require("bolt11");
+const bodyParser = require("body-parser");
+const {
+  relayInit,
+  getPublicKey,
+  getEventHash,
+  getSignature,
+} = require("nostr-tools");
+const {
+  GPT_SCHEMA,
+  GPT_RESULT_SCHEMA,
+  STABLE_DIFFUSION_SCHEMA,
+  STABLE_DIFFUSION_RESULT_SCHEMA,
+  OFFERING_KIND,
+} = require("./lib/defines.js");
+const { sleep } = require("./lib/helpers");
+
+require("dotenv").config();
+global.WebSocket = WebSocket;
+
+const mongoose = require("mongoose");
+
+// --------------------- MONGOOSE -----------------------------
+
+const JobRequestSchema = new mongoose.Schema({
+  invoice: Object,
+  paymentHash: String,
+  verifyURL: String,
+  status: String,
+  result: String,
+  price: Number,
+  requestData: Object,
+  requestResponse: Object,
+  service: String,
+  state: String,
+});
+
+const JobRequest = mongoose.model("JobRequest", JobRequestSchema);
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB!");
+});
+
+// --------------------- HELPERS -----------------------------
+
+function logState(service, paymentHash, state) {
+  console.log(`${paymentHash.substring(0, 5)} - ${service}: ${state}`);
+}
+
+function getLNURL() {
+  const parts = process.env.LN_ADDRESS.split("@");
+  if (parts.length !== 2) {
+    throw new Error(`Invalid lnAddress: ${process.env.LN_ADDRESS}`);
+  }
+  const username = parts[0];
+  const domain = parts[1];
+  return `https://${domain}/.well-known/lnurlp/${username}`;
+}
+
+async function createNewJobDocument(service, invoice, paymentHash, price) {
+  const newDocument = new JobRequest({
+    invoice,
+    paymentHash,
+    verifyURL: invoice.verify,
+    price,
+    service,
+    status: "UNPAID",
+    result: null,
+    requestData: null,
+  });
+
+  // Save the document to the collection
+  await newDocument.save();
+}
+
+async function findJobRequestByPaymentHash(paymentHash) {
+  const jobRequest = await JobRequest.findOne({ paymentHash }).exec();
+  if (!jobRequest) {
+    throw new Error("No Doc found");
+  }
+
+  return jobRequest;
+}
+
+async function getIsInvoicePaid(paymentHash) {
+  const doc = await findJobRequestByPaymentHash(paymentHash);
+
+  const invoice = doc.invoice;
+
+  if (doc.status == "PAID") {
+    return { isPaid: true, invoice };
+  }
+
+  const response = await axios.get(doc.verifyURL, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const isPaid = response.data.settled == true;
+
+  doc.status = isPaid ? "PAID" : doc.status;
+  await doc.save();
+
+  return { isPaid, invoice };
+}
+
+async function getPaymentHash(invoice) {
+  const decodedInvoice = await bolt11.decode(invoice);
+  const paymentHashTag = decodedInvoice.tags.find(
+    (tag) => tag.tagName === "payment_hash"
+  ).data;
+  return paymentHashTag;
+}
+
+async function generateInvoice(service) {
+  const msats = getServicePrice(service);
+  const lnurlResponse = await axios.get(getLNURL(), {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const lnAddress = lnurlResponse.data;
+
+  if (msats > lnAddress.maxSendable || msats < lnAddress.minSendable) {
+    throw new Error(
+      `${msats} msats not in sendable range of ${lnAddress.minSendable} - ${lnAddress.maxSendable}`
+    );
+  }
+
+  const expiration = new Date(Date.now() + 3600 * 1000); // One hour from now
+  const url = `${lnAddress.callback}?amount=${msats}&expiry=${Math.floor(
+    expiration.getTime() / 1000
+  )}`;
+
+  const invoiceResponse = await axios.get(url);
+  const invoiceData = invoiceResponse.data;
+
+  const paymentHash = await getPaymentHash(invoiceData.pr);
+  const successAction = getSuccessAction(service, paymentHash);
+
+  const invoice = { ...invoiceData, successAction, paymentHash };
+
+  await createNewJobDocument(service, invoice, paymentHash, msats);
+
+  return invoice;
+}
+
+function getSuccessAction(service, paymentHash) {
+  return {
+    tag: "url",
+    url: `http://localhost:3000/${service}/${paymentHash}/get_result`,
+    description: "Open to get the confirmation code for your purchase.",
+  };
+}
+
+// --------------------- ENDPOINTS -----------------------------
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.post("/:service", async (req, res) => {
+  try {
+    const service = req.params.service;
+    const invoice = await generateInvoice(service);
+    const doc = await findJobRequestByPaymentHash(invoice.paymentHash);
+
+    doc.requestData = req.body;
+    doc.state = "NOT_PAID";
+    await doc.save();
+
+    logState(service, invoice.paymentHash, "REQUESTED");
+
+    res.status(402).send(invoice);
+  } catch (e) {
+    console.log(e.toString().substring(0, 150));
+    res.status(500).send(e);
+  }
+});
+
+app.get("/:service/:payment_hash/get_result", async (req, res) => {
+  try {
+    const service = req.params.service;
+    const paymentHash = req.params.payment_hash;
+    const { isPaid, invoice } = await getIsInvoicePaid(paymentHash);
+
+    logState(service, paymentHash, "POLL");
+    if (isPaid != true) {
+      res.status(402).send({ ...invoice, isPaid });
+    } else {
+      const doc = await findJobRequestByPaymentHash(paymentHash);
+
+      switch (doc.state) {
+        case "WORKING":
+          logState(service, paymentHash, "WORKING");
+          res.status(202).send({ state: doc.state });
+          break;
+        case "ERROR":
+        case "DONE":
+          logState(service, paymentHash, doc.state);
+          res.status(200).send(doc.requestResponse);
+          break;
+        default:
+          logState(service, paymentHash, "PAID");
+          const data = doc.requestData;
+          submitService(service, data)
+            .then(async (response) => {
+              doc.requestResponse = response;
+              doc.state = "DONE";
+              await doc.save();
+            })
+            .catch(async (e) => {
+              doc.requestResponse = e;
+              doc.state = "ERROR";
+              await doc.save();
+            });
+
+          doc.state = "WORKING";
+          await doc.save();
+          res.status(202).send({ state: doc.state });
+      }
+    }
+  } catch (e) {
+    console.log(e.toString().substring(0, 300));
+    res.status(500).send(e);
+  }
+});
+
+app.get("/:service/:payment_hash/check_payment", async (req, res) => {
+  try {
+    const paymentHash = req.params.payment_hash;
+    const { isPaid, invoice } = await getIsInvoicePaid(paymentHash);
+
+    res.status(200).json({ invoice, isPaid });
+  } catch (e) {
+    console.log(e.toString().substring(0, 50));
+    res.status(500).send(e);
+  }
+});
+
+// --------------------- SERVICES -----------------------------
+
+function getServicePrice(service) {
+  switch (service) {
+    case "GPT":
+      return process.env.GPT_MSATS;
+    case "STABLE":
+      return process.env.STABLE_DIFFUSION_MSATS;
+    default:
+      return process.env.GPT_MSATS;
+  }
+}
+
+function submitService(service, data) {
+  switch (service) {
+    case "GPT":
+      return callChatGPT(data);
+    case "STABLE":
+      return callStableDiffusion(data);
+    default:
+      return callChatGPT(data);
+  }
+}
+
+async function callChatGPT(data) {
+  var config = {
+    method: "post",
+    url: "https://api.openai.com/v1/chat/completions",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.CHAT_GPT_API_KEY}`,
+    },
+    data,
+  };
+
+  try {
+    const response = await axios(config);
+    return response.data;
+  } catch (e) {
+    console.log(`ERROR: ${e.toString().substring(0, 50)}`);
+    return e;
+  }
+}
+
+async function callStableDiffusion(data) {
+  const newData = {
+    ...data,
+    key: process.env.STABLE_DIFFUSION_API_KEY,
+  };
+
+  const config = {
+    method: "post",
+    url: "https://stablediffusionapi.com/api/v4/dreambooth",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: newData,
+  };
+
+  try {
+    let isProcessing = true;
+    let response;
+    while (isProcessing) {
+      response = await axios(config);
+      if (response.data.status !== "processing") isProcessing = false;
+      await sleep(1000);
+    }
+
+    return response.data;
+  } catch (e) {
+    console.log(`ERROR: ${e.toString().substring(0, 50)}`);
+    return e;
+  }
+}
+
+// --------------------- NOSTR -----------------------------
+function createOfferingNote(
+  pk,
+  sk,
+  service,
+  cost,
+  endpoint,
+  status,
+  inputSchema,
+  outputSchema,
+  description
+) {
+  const now = Math.floor(Date.now() / 1000);
+
+  const content = {
+    endpoint, // string
+    status, // UP/DOWN/CLOSED
+    cost, // number
+    inputSchema, // Json Schema
+    outputSchema, // Json Schema
+    description, // string / NULL
+  };
+
+  let offeringEvent = {
+    kind: OFFERING_KIND,
+    pubkey: pk,
+    created_at: now,
+    tags: [
+      ["s", service],
+      ["d", service],
+    ],
+    content: JSON.stringify(content),
+  };
+  offeringEvent.id = getEventHash(offeringEvent);
+  offeringEvent.sig = getSignature(offeringEvent, sk);
+
+  return offeringEvent;
+}
+
+// Post Offerings
+async function postOfferings() {
+  const sk = process.env.NOSTR_SK;
+  const pk = getPublicKey(sk);
+
+  const relay = relayInit(process.env.NOSTR_RELAY);
+  relay.on("connect", () => {
+    console.log(`connected to ${relay.url}`);
+  });
+  relay.on("error", (e) => {
+    console.log(`failed to connect to ${relay.url}: ${e}`);
+  });
+  await relay.connect();
+
+  const gptOffering = createOfferingNote(
+    pk,
+    sk,
+    "https://api.openai.com/v1/chat/completions",
+    Number(process.env.GPT_MSATS),
+    process.env.ENDPOINT + "/" + "GPT",
+    "UP",
+    GPT_SCHEMA,
+    GPT_RESULT_SCHEMA,
+    "Get your GPT needs here!"
+  );
+
+  await relay.publish(gptOffering);
+  console.log(`Published GPT Offering: ${gptOffering.id}`);
+
+  const sdOffering = createOfferingNote(
+    pk,
+    sk,
+    "https://stablediffusionapi.com/api/v4/dreambooth",
+    Number(process.env.STABLE_DIFFUSION_MSATS),
+    process.env.ENDPOINT + "/" + "STABLE",
+    "UP",
+    STABLE_DIFFUSION_SCHEMA,
+    STABLE_DIFFUSION_RESULT_SCHEMA,
+    "Get your SD needs here!"
+  );
+
+  await relay.publish(sdOffering);
+  console.log(`Published Stable Diffusion Offering: ${sdOffering.id}`);
+
+  relay.close();
+}
+
+postOfferings();
+
+// --------------------- SERVER -----------------------------
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Starting NST Backend...");
+  console.log(`Server started on port ${port}.`);
+});
